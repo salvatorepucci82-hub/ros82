c5c2792ec4c82d258cd0bb15e1a247407e6b2c9f
salvatorepucci82-hub <salvatorepucci82@gmail.com>
2025-08-15 17:34:22 +0200

    Add files via upload
    Initial upload: progetto Pavimentazione completo

---
+++ lib/screens/login.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final emailController = TextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Login")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(labelText: "Email"),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (_) => const HomeScreen()),
+                );
+              },
+              child: const Text("Entra"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
